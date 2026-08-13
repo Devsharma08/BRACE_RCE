274c94abf546478a05ad4abd904a5dc825f2ab27
DevSharma08 <devsharma08072004@gmail.com>
2026-08-13 16:23:13 +0530

    chore: remove temporary debug and cleanup scripts

--- server/test-snippets.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function run() {
-    const snippets = await prisma.codeSnippet.findMany({ select: { wrapperCode: true, problem: { select: { name: true } }, language: true } });
-    console.log(snippets.filter(s => s.language === 'javascript' && s.wrapperCode && s.wrapperCode.includes('Wrapper')));
-}
-run();
